2ba1df90176a668f51d56ad33ce47c781b620ae2
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-15 12:59:48 -0700

    put example state comment in root reducer file

--- client/scripts/reducers/index.js
+++ client/scripts/reducers/index.js
@@ -3,3 +3,35 @@ import { combineReducers } from 'redux'
 const rootReducer = combineReducers({});
 
 export default rootReducer
+
+
+/*
+
+exapmle state:
+
+state = {
+  ui: {
+    isLandlord: boolean,
+    currentView: string,
+    username: string,
+    house: {
+      code: string,
+      name: string
+    }
+  },
+  context: {
+    chores: Array of chore objects,
+    finances: {
+      payment: Array of payment objects,
+      bills: Array of bill objects
+    },
+    landlordMessages: Array of message objects,
+    counters: {
+      messages: Number,
+      landlordMessages: Number,
+      finances: Number
+    }
+  }
+}
+
+*/
