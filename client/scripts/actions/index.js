59d2110860c8857b22f1a1fa1e868195374f81ad
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-15 20:33:40 -0700

    update action creater to change view

--- client/scripts/actions/index.js
+++ client/scripts/actions/index.js
@@ -1,8 +1,10 @@
+import { viewsObject } from '../components/config/views'
+
 // change the current view when clicking on a 
 // tab in the navbar
 export const changeView = (view) => {
   return {
     type: 'CHANGE_VIEW',
-    payload: view
+    payload: viewsObject[view]
   }
 }
